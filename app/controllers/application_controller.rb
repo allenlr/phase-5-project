11c9bb6499aa62b001fd68f09e2625142479b61a
allenlr <allenlru55@gmail.com>
2023-10-20 18:31:25 -0500

    create and test basic index and show routes for users along with corresponding actions in users controller. create recordinvalid and recordnotfound exception rescues in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    private
+
+    def unprocessable_entity(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def not_found
+        render json: { error: "Record not found" }, status: :not_found
+    end
+
 end
