4b33d244e39d04f488b2bb67736f440901ede0ce
allenlr <allenlru55@gmail.com>
2023-10-28 20:58:10 -0500

    set up reviewable for reviews to determine path from user or service_provider for which to access reviews from, ensure reviews are returned by accessing path to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+    before_action :set_reviewable
+
+    def index
+        reviews = @reviewable.reviews
+        render json: reviews, status: :ok
+    end
+
+    def show
+        review = @reviewable.reviews.find(params[:id])
+        render json: review, status: :ok
+    end
+
+    private
+
+    def set_reviewable
+        @reviewable = if params[:user_id]
+            User.find(params[:user_id])
+        elsif params[:service_provider_id]
+            ServiceProvider.find(params[:service_provider_id])
+        end
+    end
+
+    def review_params
+        params.permit(:comment, :rating, :user_id, :service_provider_id)
+    end
+
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,7 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :rating, :user_id, :service_provider_id, :created_at
+  attributes :id, :comment, :rating, :user_id, :service_provider_id, :created_at, :username
+
+  def username
+    object.user[:username]
+  end
 end
