3350ae100dda2ccbe0592f22e2492ce911546dac
allenlr <allenlru55@gmail.com>
2023-11-08 18:53:16 -0600

    add text area and buttons for writing a new review

--- client/src/components/Services/ServiceProvider.js
+++ client/src/components/Services/ServiceProvider.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react'
 import './Services.css'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
-import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 import Review from './Review';
 
 function ServiceProvider({provider}){
     // const reviews = Array.isArray(provider.reviews) ? provider.reviews : [];
     const [reviews, setReviews] = useState(provider.reviews || [])
     const [showDetails, setShowDetails] = useState(false)
+    const currentUser = useSelector(state => state.user.currentUser)
+    const [newReview, setNewReview] = useState("")
+    const [writeReview, setWriteReview] = useState(false)
 
-    console.log(provider)
+    function handleReviewCancel(){
+        setNewReview("")
+        setWriteReview(false)
+    }
+
     return(
         <div>
             <h3 className='provider-names' onClick={() => setShowDetails((prev) => !prev)}>
@@ -18,9 +24,20 @@ function ServiceProvider({provider}){
             <p className='provider-descriptions'>
                 {provider.description}
             </p>
+            
             {showDetails && (
                 <div>
                     <h4 className="provider-reviews-header">{`Reviews (${reviews.length})`}</h4>
+                    <button className="write-review-button" onClick={() => setWriteReview((prevValue) => !prevValue)}>Write Review</button>
+                    {currentUser && writeReview && (
+                        <div>
+                            <textarea placeholder="type review..." value={newReview} onChange={(e) => setNewReview(e.target.value)}></textarea>
+                            <div className="save-cancel-wrapper">
+                                    <button className="save-cancel-edit-buttons">Post</button>
+                                    <button className="save-cancel-edit-buttons" onClick={handleReviewCancel}>Cancel</button>
+                            </div>
+                        </div>
+                    )}
                     {reviews.map((review) => {
                         return (
                             <Review key={review?.id} review={review} reviewsList={reviews} providerId={provider?.id} setReviewsList={setReviews}/>
