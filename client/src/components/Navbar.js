024701fa20bbb82ea9b14c03b55967969d5e8dca
allenlr <allenlru55@gmail.com>
2023-11-22 01:21:22 -0600

    clear error when clicking on navlinks

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,27 +2,30 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Account from './User/Account';
+import { setError } from './errorSlice';
+import { useDispatch } from 'react-redux';
 
 
 function Navbar(){
     const currentUser = useSelector(state => state.user.currentUser);
+    const dispatch = useDispatch();
 
 
     return (
         <nav className="navbar">
-            <Link to="/" className="navbar-brand">
+            <Link to="/" className="navbar-brand" onClick={() => dispatch(setError(null))}>
                 Home
             </Link>
-            <Link to="/services" className="nav-link">
+            <Link to="/services" className="nav-link" onClick={() => dispatch(setError(null))}>
                 Search Services
             </Link>
             {currentUser ? 
-                <div className="navbar-actions">
+                <div className="navbar-actions" onClick={() => dispatch(setError(null))}>
                     <Account/>
                     
                 </div>
                 :
-                <Link to="/login" className="nav-link">
+                <Link to="/login" className="nav-link" onClick={() => dispatch(setError(null))}>
                     Login/Register
                 </Link>
             }
